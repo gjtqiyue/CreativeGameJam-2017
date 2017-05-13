fa238aecede6a7d7fff80d0a064f277a882f8fce
gjtqiyue <gjtqiyue@gmail.com>
2017-05-13 12:59:51 -0400

    Endgame conditions structure

--- Amuse-gueule/Assets/Scripts/GameManagerScript.cs
+++ Amuse-gueule/Assets/Scripts/GameManagerScript.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class GameManagerScript : MonoSingleton<GameManagerScript>
 {
-	// Use this for initialization
+
+	public float timeDuration;
+	public int numOfBugs;
+	public int remainingNumOfBugs;
+
+	private float gameTimer;
+
 	void Start ()
 	{
-
+		remainingNumOfBugs = numOfBugs;
+		timeDuration = 10;
+		gameTimer = timeDuration;
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
-		
+		gameTimer -= Time.deltaTime;
+		//end condition 1: time is up
+		if (gameTimer <= 0)
+		{
+			//pop up a text to say the game over
+			//go to the scoreboard
+			//ask restart? or quit
 	}
+		//end condition 2: eat all the things
+		if (some function == 0)
+
 }
