ba65aad2c098583aa68bbb4e275afbd66d390d57
Kévin Duguay <kevin.duguay1@gmail.com>
2017-05-12 21:13:25 -0400

    Prepared main scene and folders

---
+++ Amuse-gueule/Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public abstract class Singleton<T> where T : class
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = CreateInstanceOfT();
+            return _instance;
+        }
+    }
+
+    private static T CreateInstanceOfT()
+    {
+        return Activator.CreateInstance(typeof(T), true) as T;
+    }
+}
