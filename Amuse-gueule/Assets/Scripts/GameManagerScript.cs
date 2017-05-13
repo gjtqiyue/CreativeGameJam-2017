4fcce29de0967ad483bd9f7cb9aebe77e7c92343
tonystrawberry <tonyduong@live.fr>
2017-05-13 13:33:44 -0400

    persistent score script added

--- Amuse-gueule/Assets/Scripts/GameManagerScript.cs
+++ Amuse-gueule/Assets/Scripts/GameManagerScript.cs
@@ -5,30 +5,36 @@ using UnityEngine;
 public class GameManagerScript : MonoSingleton<GameManagerScript>
 {
 
-	public float timeDuration;
-	public int numOfBugs;
-	public int remainingNumOfBugs;
+    public float timeDuration;
+    public int numOfBugs;
+    public int remainingNumOfBugs;
 
-	private float gameTimer;
+    private float gameTimer;
 
-	void Start ()
-	{
-		remainingNumOfBugs = numOfBugs;
-		timeDuration = 10;
-		gameTimer = timeDuration;
-	}
-	
+    void Start()
+    {
+        remainingNumOfBugs = numOfBugs;
+        gameTimer = timeDuration;
+    }
 
-	void Update () {
-		gameTimer -= Time.deltaTime;
-		//end condition 1: time is up
-		if (gameTimer <= 0)
-		{
-			//pop up a text to say the game over
-			//go to the scoreboard
-			//ask restart? or quit
-	}
-		//end condition 2: eat all the things
-		if (some function == 0)
 
+    void Update()
+    {
+        gameTimer -= Time.deltaTime;
+        //end condition 1: time is up
+        if (gameTimer <= 0)
+        {
+            //pop up a text to say the game over
+            //go to the scoreboard
+            //ask restart? or quit
+        }
+        //end condition 2: eat all the things
+        if (remainingNumOfBugs == 0)
+        {
+            //pop up a text to say the game over
+            //go to the scoreboard
+            //ask restart? or quit
+        }
+
+    }
 }
---
+++ Amuse-gueule/Assets/Scripts/ScoreSaveLoad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class ScoreSaveLoad : Singleton<ScoreSaveLoad> {
+
+    public static List<int> scores = new List<int>();
+
+    public static void Save(List<int> scoresList)
+    {
+        ScoreSaveLoad.scores = scoresList;
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/savedScores.gd");
+        bf.Serialize(file, ScoreSaveLoad.scores);
+        file.Close();
+    }
+
+    public static void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/savedGames.gd"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+            ScoreSaveLoad.scores = (List<int>)bf.Deserialize(file);
+            file.Close();
+        }
+    }
+}
