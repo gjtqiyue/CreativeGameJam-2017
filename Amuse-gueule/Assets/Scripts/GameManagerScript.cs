be4cfec5d3ffb6a9863f49b336af12ffd47d5b81
gjtqiyue <gjtqiyue@gmail.com>
2017-05-13 15:15:12 -0400

    Gameover and Restart text

--- Amuse-gueule/Assets/Scripts/GameManagerScript.cs
+++ Amuse-gueule/Assets/Scripts/GameManagerScript.cs
@@ -1,20 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManagerScript : MonoSingleton<GameManagerScript>
 {
 
 	public float timeDuration;
 	public int numOfBugs;
-	public int remainingNumOfBugs;
 
+	private int remainingNumOfBugs;
+	private bool raiseHeadTrigger;
 	private float gameTimer;
 
 	void Start ()
 	{
+		raiseHeadTrigger = false;
 		remainingNumOfBugs = numOfBugs;
-		timeDuration = 10;
 		gameTimer = timeDuration;
 	}
 	
@@ -22,13 +24,46 @@ public class GameManagerScript : MonoSingleton<GameManagerScript>
 	void Update () {
 		gameTimer -= Time.deltaTime;
 		//end condition 1: time is up
-		if (gameTimer <= 0)
-		{
+		if (TimeIsUp ()) {
 			//pop up a text to say the game over
 			//go to the scoreboard
 			//ask restart? or quit
-	}
+			RaiseHead ();
+			/*
+			if (Input.GetButtonDown ("Joy1ButtA") && Input.GetButtonDown ("Joy2ButtA"))
+				SceneManager.LoadScene ("Main");*/
+		}
 		//end condition 2: eat all the things
-		if (some function == 0)
+		if (NoBugsLeft ()) {
+			//pop up a text to say the game over
+			//go to the scoreboard
+			//ask restart? or quit
+			RaiseHead ();
+		}
+	}
 
+	public void RaiseHead ()
+	{
+		raiseHeadTrigger = true;
+	}
+
+	public bool getRaiseHeadTrigger(){
+		return raiseHeadTrigger;
+	}
+
+	public bool TimeIsUp ()
+	{
+		if (gameTimer <= 0)
+			return true;
+		else
+			return false;
+	}
+
+	public bool NoBugsLeft ()
+	{
+		if (remainingNumOfBugs == 0)
+			return true;
+		else
+			return false;
+	}
 }
